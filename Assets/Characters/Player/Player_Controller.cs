2e593ac8bf89b0cb6c79cd3400a955273928440a
creatorsg <soninseon2004@gmail.com>
2025-07-07 00:50:07 +0900

    22
    2
    
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity

--- Assets/Characters/Player/Player_Controller.cs
+++ Assets/Characters/Player/Player_Controller.cs
@@ -231,6 +231,11 @@ namespace Player
 
         }
 
+        void Attack()
+        {
+
+        }
+
         void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.gameObject.CompareTag("Ground"))
