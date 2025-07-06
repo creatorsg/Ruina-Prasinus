63cf71bbd94a4b53d67c10cb41fad344d2b5840a
creatorsg <soninseon2004@gmail.com>
2025-07-07 00:50:07 +0900

    22
    2

--- Assets/Characters/Player/Player_Controller.cs
+++ Assets/Characters/Player/Player_Controller.cs
@@ -240,6 +240,11 @@ namespace Player
 
         }
 
+        void Attack()
+        {
+
+        }
+
         void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.gameObject.CompareTag("Ground"))
