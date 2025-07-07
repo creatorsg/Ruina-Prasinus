a2e0287d5d749029d9ffaaf9082d0c89dfe8da5c
creatorsg <soninseon2004@gmail.com>
2025-07-07 23:28:01 +0900

    적 및 장풍 피격판정

---
+++ Assets/Characters/Player/JangpungProjectile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class JangpungProjectile : MonoBehaviour
+{
+    private float autoDestroyTime = 0.4f;
+
+    void Start()
+    {
+        Destroy(gameObject, autoDestroyTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground") || other.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
