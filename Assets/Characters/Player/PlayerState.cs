ace2c9d8edfc92c1b1e73722400c3486cf9e6f9e
creatorsg <soninseon2004@gmail.com>
2025-07-17 03:12:02 +0900

    233

--- Assets/Characters/Player/PlayerState.cs
+++ Assets/Characters/Player/PlayerState.cs
@@ -1,8 +1,54 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerState : MonoBehaviour
 {
+    [SerializeField] private PlayerData playerData;
+
     public bool IsGrounded { get; private set; }
+    public bool IsHited { get; private set; }
+    public bool isInvincible { get; private set; }
+    public bool isDashing { get; private set; }
+
+    public UnityEvent OnHitStarted;
+    public UnityEvent OnInvincibleStarted;
+
+    private float freezeTimer = 0f;
+    private float invincibleTimer = 0f;
+
+    private void Update()
+    {
+        if(IsHited)
+        {
+            freezeTimer += Time.deltaTime;
+            if (freezeTimer >= playerData.freezeDuration)
+            {
+                IsHited = false;
+                freezeTimer = 0f;
+
+                isInvincible = true;
+                invincibleTimer = 0f;
+            }
+            else if (isInvincible)
+            {
+                invincibleTimer += Time.deltaTime;
+                if (invincibleTimer >= playerData.invincibleDuration)
+                {
+                    isInvincible = false;
+                    invincibleTimer = 0f;
+                }
+            }
+        }
+    }
+
+    public void BeginHit()
+    {
+        if (IsHited || isInvincible) return;
+
+        IsHited = true;
+        freezeTimer = 0f;
+        OnHitStarted?.Invoke();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,4 +61,6 @@ public class PlayerState : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
             IsGrounded = false;
     }
+
+
 }
