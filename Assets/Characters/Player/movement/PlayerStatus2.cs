03669be5f91a5e2bc525b36e0a81e1449079b4c6
creatorsg <soninseon2004@gmail.com>
2025-07-22 02:19:17 +0900

    경사면 수정 남음

--- Assets/Characters/Player/movement/PlayerStatus2.cs
+++ Assets/Characters/Player/movement/PlayerStatus2.cs
@@ -1,25 +1,35 @@
 using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerStatus2
 {
-
     public bool isGround { get; private set; }
-    public bool isHit { get; private set; }
-    public bool isInvincible { get; private set; }
+    public bool isSlope { get; private set; }
+
+    public Vector2 perp;
+    private float angle;
 
     public void Update(Transform Player, float checkRadius, LayerMask groundMask)
     {
         GroundCheck(Player, checkRadius, groundMask);
-
     }
     void GroundCheck(Transform Player, float checkRadius, LayerMask ground)
     {
         isGround = Physics2D.OverlapCircle(Player.position, checkRadius, ground);
+        Debug.Log(isGround);
     }
 
-    void HitStart(PlayerState state, Enemy enemy)
+    public void SlopeCheck(RaycastHit2D hit)
     {
+        perp = Vector2.Perpendicular(hit.normal);
+        angle = Vector2.Angle(hit.normal, Vector2.up);
 
+        Debug.Log("히트 체크중");
+
+        if (angle != 0)
+            isSlope = true;
+        else
+            isSlope = false;
     }
 }
---
+++ Assets/Characters/Player/movement/newMovementController.cs
@@ -0,0 +1,73 @@
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.UIElements;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class newMovementController : MonoBehaviour
+{
+    [SerializeField] private PlayerState state;
+    [SerializeField] private InputComponent input;
+    [SerializeField] private newPlayerView view;
+    
+    private PlayerStatus2 status;
+    private Modeldeldel model;
+
+    [HideInInspector] public RaycastHit2D hit, fronthit;
+    private bool dashRequested, jumpRequested;
+    public Transform Player, frontCheck;
+    private float moveInput;
+    public float checkRadius;
+    private float distance = 1;
+    private LayerMask groundMask;
+
+    void Awake()
+    {
+        model = new Modeldeldel();
+        status = new PlayerStatus2();
+        groundMask = LayerMask.GetMask("Ground");
+        input = GetComponent<InputComponent>();
+
+        input.OnMove += dir => moveInput = dir;
+        input.OnDash += () => dashRequested = true;
+        input.OnJump += () => jumpRequested = true;
+
+        view.SetModel(model);
+        view.SetState(state);
+
+    }
+
+    private void FixedUpdate()
+    {
+        view.Move(moveInput, model.moveSpeed, status.perp);
+
+        if (jumpRequested && status.isGround)
+        {
+            view.Jump();
+        }
+    }
+
+    void Update()
+    {
+        bool dashHeld = InputManager.GetKey("Dash");
+
+        hit = Physics2D.Raycast(Player.position, Vector2.down, distance, groundMask);
+        fronthit = Physics2D.Raycast(frontCheck.position, transform.right, 0.1f, groundMask);
+
+        status.Update(Player, checkRadius, groundMask);
+        model.Update(moveInput, dashRequested, dashHeld, state, Time.deltaTime, status.isGround);
+
+        if (hit || fronthit)
+        {
+            if (fronthit)
+                status.SlopeCheck(fronthit);
+            else if (hit)
+                status.SlopeCheck(hit);
+        }
+
+        dashRequested = false;
+        jumpRequested = false;
+    }
+}
