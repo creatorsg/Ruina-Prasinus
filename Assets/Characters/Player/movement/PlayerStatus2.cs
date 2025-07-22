0b174e72370cd9965bb1d0e8962bb0872b3177c4
creatorsg <soninseon2004@gmail.com>
2025-07-22 20:18:52 +0900

    중간 저장

--- Assets/Characters/Player/movement/PlayerStatus2.cs
+++ Assets/Characters/Player/movement/PlayerStatus2.cs
@@ -17,15 +17,12 @@ public class PlayerStatus2
     void GroundCheck(Transform Player, float checkRadius, LayerMask ground)
     {
         isGround = Physics2D.OverlapCircle(Player.position, checkRadius, ground);
-        Debug.Log(isGround);
     }
 
     public void SlopeCheck(RaycastHit2D hit)
     {
         perp = Vector2.Perpendicular(hit.normal);
         angle = Vector2.Angle(hit.normal, Vector2.up);
-
-        Debug.Log("히트 체크중");
 
         if (angle != 0)
             isSlope = true;
