5b20d597842314b3f89d7e02084ea1156a47de5c
WagaTonoka <hdh060209@gmail.com>
2025-08-19 03:10:59 +0900

    parallaxBg 수정
    ㅍㅍ

--- Assets/Game/Camera/ParallaxBackground.cs
+++ Assets/Game/Camera/ParallaxBackground.cs
@@ -1,24 +1,23 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public struct BG_STR
+{
+    [SerializeField] public Transform backgroundTransform;
+    [SerializeField] public float parallaxFactorX;
+    [SerializeField] public float parallaxFactorY;
+}
 
 public class ParallaxBackground : MonoBehaviour
 {
     [SerializeField] private Transform cameraTransform;
-    [SerializeField] private float parallaxFactor = 0.5f;
-
-    [SerializeField] private bool moveX;
-    [SerializeField] private bool moveY;
-    [SerializeField] private bool isTest;
-
-    private void Start()
-    {
-        if (cameraTransform == null)
-            cameraTransform = Camera.main.transform;
-    }
+    [SerializeField] private List<BG_STR> BG_List;
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(cameraTransform.position.x * (moveX ? parallaxFactor : 1), cameraTransform.position.y * (moveY ? parallaxFactor : 1), 0);
-        if (isTest)
-            Debug.Log(transform.position.x);
+        foreach (BG_STR bg in BG_List)
+        bg.backgroundTransform.position = new Vector3(cameraTransform.position.x * bg.parallaxFactorX, cameraTransform.position.y * bg.parallaxFactorY, 0);
     }
 }
